07d1732c05fbba5cfe5a3e13379417a44478aca6
Попов Евгений Александрович <PopovEA@plast.local>
2017-08-09 15:31:14 +0300

    Calculate create

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,31 @@
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Calculate");
+		
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[2]);
+		
+		int result;
+		
+		switch (args[1]){
+            case "\u002B":
+                System.out.println("Summ: " + (first + second));
+                break;
+            case "\u002D":
+                System.out.println("Raznost: " + (first - second));
+                break;
+            case "p":
+                System.out.println("Proizvedenie: " + (first * second));
+                break;
+            case "d":
+                System.out.println("Delenie: " + (float)(first / second));
+                break;
+        }
+		
+		
+		
+		
+		
+		
 	}
 }
